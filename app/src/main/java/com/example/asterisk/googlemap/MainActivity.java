7a84463e2635278d202d28ba2f4afcefd1b2c8b7
beknot <chakradharbinod200@gmail.com>
2018-05-15 23:08:48 +0545

    first commit

--- app/src/main/java/com/example/asterisk/googlemap/MainActivity.java
+++ app/src/main/java/com/example/asterisk/googlemap/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     GoogleMap map;
     RequestQueue requestQueue;
     String incompleteURL="http://maps.googleapis.com/maps/api/geocode/json?address=";
+    String address;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,10 +46,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         btnnormal = findViewById(R.id.normal);
         btnterrain = findViewById(R.id.terrain);
 
-
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapfragment);
         mapFragment.getMapAsync(this);
-
 
     }
 
@@ -61,7 +60,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(LatLng latLng) {
-                clicklatlan =latLng;
+                clicklatlan = latLng;
                 googleMap.addMarker(new MarkerOptions().position(latLng).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
             }
         });
@@ -69,7 +68,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         googleMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
             @Override
             public void onMapLongClick(LatLng latLng) {
-                PolylineOptions options =new PolylineOptions();
+                PolylineOptions options = new PolylineOptions();
                 options.add(clicklatlan);
                 options.add(latLng);
                 googleMap.addPolyline(options);
@@ -77,55 +76,52 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
             }
         });
+
         btngo.setOnClickListener(new View.OnClickListener() {
-                                     @Override
-                                     public void onClick(View v) {
-                                         String search = etsearchbar.getText().toString();
-                                         incompleteURL = incompleteURL + search;
-                                         requestQueue = Volley.newRequestQueue(MainActivity.this);
-                                         StringRequest sr = new StringRequest(Request.Method.GET, incompleteURL, new Response.Listener<String>() {
-                                             @Override
-                                             public void onResponse(String response) {
-                                                 try {
-                                                     JSONObject obj1 = new JSONObject(response);
-                                                     JSONArray ar1 = obj1.getJSONArray("results");
-                                                     JSONObject obj2 = ar1.getJSONObject(0);
-                                                     JSONObject obj3 = obj2.getJSONObject("geometry");
-
-                                                     JSONObject obj4 = obj3.getJSONObject("location");
-                                                     Double lat = obj4.getDouble("lat");
-                                                     Double lng = obj4.getDouble("lng");
-                                                     LatLng l = new LatLng(lat, lng);
-                                                     map.moveCamera(CameraUpdateFactory.newLatLngZoom(l,15));
-                                                     map.addMarker(new MarkerOptions().position(l).title("Marked item"));
-                                                 } catch (Exception e) {
-                                                 }
-                                             }
-                                         }, new Response.ErrorListener() {
-                                             @Override
-                                             public void onErrorResponse(VolleyError error) {
-
-                                             }
-                                         });
-                                         requestQueue.add(sr);
-                                     }
-                                 });
-                btnnormal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                address = etsearchbar.getText().toString();
+                address = address.replace(" ","_");
+                String search = incompleteURL + address;
+                requestQueue = Volley.newRequestQueue(MainActivity.this);
+                StringRequest sr = new StringRequest(Request.Method.GET, search, new Response.Listener<String>() {
                     @Override
-                    public void onClick(View v) {
-                        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-                    }
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject obj1 = new JSONObject(response);
+                            JSONArray ar1 = obj1.getJSONArray("results");
+                            JSONObject obj2 = ar1.getJSONObject(0);
+                            JSONObject obj3 = obj2.getJSONObject("geometry");
+                            JSONObject obj4 = obj3.getJSONObject("location");
+                            Double lat = obj4.getDouble("lat");
+                            Double lng = obj4.getDouble("lng");
+                            LatLng l = new LatLng(lat, lng);
+                            map.moveCamera(CameraUpdateFactory.newLatLngZoom(l,15));
+                            map.addMarker(new MarkerOptions().position(l).title("Marked item"));
+                            } catch (Exception e) {
+                            }
+                            }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        }
+                        });
+                requestQueue.add(sr);
+                }
                 });
-                btnsatellite.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-                    }
+            btnnormal.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                }});
+            btnsatellite.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    map.setMapType(GoogleMap.MAP_TYPE_SATELLITE); }
                 });
-                btnterrain.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        map.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
-                    }
+            btnterrain.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    map.setMapType(GoogleMap.MAP_TYPE_TERRAIN); }
                 });
             }}
